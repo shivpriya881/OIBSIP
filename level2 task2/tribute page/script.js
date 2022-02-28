dadf6457c73291bbe7ac71a10333ed79e316dcd7
shivpriya881 <83631666+shivpriya881@users.noreply.github.com>
2022-02-28 22:33:22 +0530

    Add files via upload

---
+++ level2 task2/tribute page/script.js
@@ -0,0 +1,36 @@
+window.onload=function()
+{
+    document.querySelector(".content").classList.add("active");
+    
+}
+var t=false;
+function drop()
+{
+
+    document.querySelector(".bio p").classList.toggle("active");
+    if(t===false)
+    {
+        t=true;
+        document.querySelector(".bio h2 i").classList.remove("fa-caret-down");
+        document.querySelector(".bio h2 i").classList.add("fa-sort-up");
+    }
+    else{
+        t=false;
+        document.querySelector(".bio h2 i").classList.remove("fa-sort-up");
+        document.querySelector(".bio h2 i").classList.add("fa-caret-down");
+        
+    }
+    document.querySelector(".quotes p").classList.toggle("active");
+    if(t===false)
+    {
+        t=true;
+        document.querySelector(".quotes h2 i").classList.remove("fa-caret-down");
+        document.querySelector(".quotes h2 i").classList.add("fa-sort-up");
+    }
+    else{
+        t=false;
+        document.querySelector(".quotes h2 i").classList.remove("fa-sort-up");
+        document.querySelector(".quotes h2 i").classList.add("fa-caret-down");
+        
+    }
+}
